3e31286b945c17e13efeee566176796e40a72f15
Tewodros Wondimu <tewodros.wr@gmail.com>
2017-10-18 22:55:40 +0200

    Completed Lesson 6

--- WorldTracking/ViewController.swift
+++ WorldTracking/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import ARKit // Allows you to use the ARKit Framework
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    // Used to track position and orientation of your phone
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Debug options allows us
+        // Show feature points shows the points in your view
+        // Show world origin shows where the origin of the world is
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin];
+        
+        // Make the sceneview run the configurations
+        self.sceneView.session.run(configuration)
     }
 
     override func didReceiveMemoryWarning() {
